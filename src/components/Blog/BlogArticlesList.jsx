a35d6bf4b3723bf2d0e55f8e4e6f8d46dcea8a4e
Your Name <contact@visualandko.com>
2024-06-16 15:18:44 +0200

    blog2

--- src/components/Blog/BlogArticlesList.jsx
+++ src/components/Blog/BlogArticlesList.jsx
@@ -1,13 +1,9 @@
-
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
-import instanceAxios from '/helpers/axios';
-import { useTranslation } from 'react-i18next';
-import '/styles/blogArticlesList.module.scss';
-
+import axios from 'axios';
+import styles from '/styles/blogArticlesList.module.scss';
 
 export default function BlogArticlesList({ categoryid }) {
-  const { i18n } = useTranslation();
   const [articlesList, setArticlesList] = useState([]);
   const router = useRouter();
 
@@ -16,22 +12,35 @@ export default function BlogArticlesList({ categoryid }) {
   };
 
   useEffect(() => {
-    instanceAxios()
-      .get(
-        `get_blog_article.php?${
-          categoryid > 0 ? `categoryid=${categoryid}` : ''
-        }&lang=${i18n.language}`,
-      )
-      .then((res) => {
-        if (res.data.success && res.data.type === 'list') {
-          setArticlesList(res.data.results);
+    const fetchArticles = async () => {
+      try {
+        const response = await axios.get(
+          `${process.env.NEXT_PUBLIC_API_URL}/get_blog_article.php`, {
+            params: {
+              categoryid: categoryid > 0 ? categoryid : undefined,
+              // lang: 'fr', // Vous pouvez remplacer 'fr' par la langue souhaitée ou par une variable dynamique
+            },
+            // headers: {
+            //   'If-None-Match': '*', // Exemple d'en-tête, adaptez en fonction des exigences du serveur
+            //   'Accept': 'application/json',
+            //   'DNT': '1',
+            //   'User-Agent': navigator.userAgent,
+            // }
+          }
+        );
+        if (response.data.success && response.data.type === 'list') {
+          setArticlesList(response.data.results);
         }
-      })
-      .catch(() => {});
-  }, [i18n.language]);
+      } catch (error) {
+        console.error("Erreur lors de la récupération des articles:", error);
+      }
+    };
+
+    fetchArticles();
+  }, [categoryid]);
 
   return (
-    <div className="articlesList">
+    <div className={styles.articlesList}>
       {articlesList.slice(0, 4).map((e, index) => (
         <div
           key={index}
@@ -39,14 +48,11 @@ export default function BlogArticlesList({ categoryid }) {
           tabIndex={0}
           onClick={() => handleClickNavigate(e.id)}
           onKeyDown={() => handleClickNavigate(e.id)}
-          read_duration={`${e.read_duration} min`}
-          className={`card article${index + 1}`}
+          className={`${styles.card} ${styles[`article${index + 1}`]}`}
         >
           <img
             alt={`blog ${index}`}
-            src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/uploads/blogs/${
-              e.img_primary
-            }`}
+            src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/uploads/blogs/${e.img_primary}`}
           />
           <h3>{e.title}</h3>
         </div>
