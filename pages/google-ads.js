88d05af974c7a8a85d1c441155f5ff7828f006f0
Your Name <contact@visualandko.com>
2024-06-20 21:04:45 +0200

    test

--- pages/google-ads.js
+++ pages/google-ads.js
@@ -16,6 +16,8 @@ const GoogleAds = () => {
     telephone: '',
   });
 
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+
   useEffect(() => {
     axios.get('/datas/forfaits.json')
       .then((res) => {
@@ -25,6 +27,29 @@ const GoogleAds = () => {
         console.error('Error fetching data:', error);
       });
   }, []);
+
+  useEffect(() => {
+    const sections = document.querySelectorAll(`.${styles.appear}`);
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        const currentScrollPos = window.pageYOffset;
+        if (entry.isIntersecting && currentScrollPos > prevScrollPos) {
+          entry.target.classList.add(styles.active);
+        }
+        setPrevScrollPos(currentScrollPos);
+      });
+    }, { threshold: 0.1 });
+
+    sections.forEach(section => {
+      observer.observe(section);
+    });
+
+    return () => {
+      sections.forEach(section => {
+        observer.unobserve(section);
+      });
+    };
+  }, [prevScrollPos]);
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -53,48 +78,128 @@ const GoogleAds = () => {
 
     setShowModal(false);
   };
+  
+    
 
   return (
     <main>
+    <Hero />
+    <div className={styles.section}>
+    <h1>Propulser votre entreprise grâce à Google ads</h1>
+    </div>
+    <main className={styles.mainga} >
       {/* <HelmetMeta title={title} description={description} /> */}
-      <Hero />
-      <section className={styles.presentation}>
-        <h2>À propos de notre service Google Ads</h2>
-        <p>
-          Chez <strong>Visual&Ko</strong>, nous sommes spécialisés dans la <strong>création</strong> et la <strong>gestion de campagnes Google Ads</strong> 
-          efficaces qui maximisent votre <strong>retour sur investissement</strong>. Notre équipe d'experts utilise des <strong>stratégies avancées</strong> 
-          pour cibler les bonnes audiences et obtenir les meilleurs résultats possibles pour votre entreprise.
-        </p>
-        <p>
-          Nous offrons une <strong>gamme complète de services</strong>, y compris :
-        </p>
-        <ul className={styles.servicesList}>
-          <li><strong>Recherche de mots-clés</strong> pour identifier les termes les plus pertinents pour votre secteur.</li>
-          <li><strong>Création d'annonces</strong> attrayantes et persuasives pour capter l'attention de vos clients potentiels.</li>
-          <li><strong>Optimisation des campagnes</strong> pour améliorer continuellement vos performances et réduire vos coûts.</li>
-          <li><strong>Analyse des performances</strong> détaillée pour vous fournir des insights clairs et exploitables.</li>
-        </ul>
-        <p>
-          Que vous soyez une <strong>petite entreprise</strong> ou une <strong>grande organisation</strong>, nous avons les outils et l'expertise pour vous aider 
-          à atteindre vos <strong>objectifs publicitaires</strong>. Faites confiance à <strong>Visual&Ko</strong> pour transformer vos investissements publicitaires en succès mesurables.
-        </p>
+      <section className={`${styles.presentation} ${styles.appear}`}>
+        <h2>Pourquoi Investir dans Google Ads ?<img className={styles.fusee} src="/images/fusée.gif" alt="Croissance des clients" /></h2>
+        <div className={`${styles.section} ${styles.appear}`}>
+          <div className={styles.text}>
+            <h3>Attirer de Nouveaux Clients et Booster Vos Ventes</h3>
+            <p>Les Google Ads sont un outil incontournable pour attirer de nouveaux clients et augmenter vos ventes en ligne. Ils permettent de générer des leads, de convertir des prospects en clients, et d’améliorer votre visibilité sur le web.</p>
+          </div>
+          <div className={styles.image}>
+            <img src="/images/adsgif.gif" alt="Croissance des clients" />
+          </div>
+        </div>
+        <div className={styles.section}>
+          <div className={styles.text}>
+            <h3>Obtenir de Nouveaux Prospects Rapidement</h3>
+            <p>Avec Google Ads, vous pouvez recevoir des appels et des formulaires de prospects intéressés par vos services en très peu de temps. Pour les sites de vente en ligne, Google Ads peut générer des ventes en moins de 24 heures.</p>
+          </div>
+        </div>
+        <div className={styles.section}>
+          <div className={styles.text}>
+            <h3>Visibilité Immédiate sur les Moteurs de Recherche</h3>
+            <p>Lorsque vous lancez une campagne Google Ads, vos annonces apparaissent en haut des résultats de recherche Google, ce qui vous permet de toucher un large public qualifié instantanément.</p>
+          </div>
+        </div>
+        <div className={styles.section}>
+          <div className={styles.text}>
+            <h3>Ciblage Précis et Pertinent</h3>
+            <p>Google Ads vous offre la possibilité de cibler vos annonces selon divers critères : mots-clés, localisation, données démographiques, intérêts et comportements d’achat. Cela vous aide à atteindre les personnes les plus susceptibles d’être intéressées par vos produits ou services.</p>
+          </div>
+        </div>
+        <div className={styles.section}>
+          <div className={styles.text}>
+            <h3>Flexibilité Budgétaire</h3>
+            <p>Avec Google Ads, vous contrôlez entièrement votre budget. Vous pouvez définir un budget quotidien ou mensuel et ne payez que lorsque quelqu’un clique sur votre annonce. Cela permet d’ajuster vos dépenses en fonction de vos besoins et objectifs.</p>
+          </div>
+        </div>
       </section>
+      <section className={`${styles.presentation} ${styles.appear}`}>
+        <h2>L'Expertise d'une Agence Google Ads <img className={styles.fusee} src="/images/fusée.gif" alt="Croissance des clients" /></h2>
+        <div className={styles.section}>
+          <div className={styles.text}>
+            <h3>Maximiser Votre Retour sur Investissement (ROI)</h3>
+            <p>L'expertise d'une agence Google Ads est essentielle pour optimiser votre ROI. Elle maîtrise les outils nécessaires pour créer des campagnes efficaces et attirer un trafic qualifié sur votre site.</p>
+            </div>
+            <div className={styles.image}>
+            <img src="/images/adsgif.gif" alt="Croissance des clients" />
+          </div>
+        </div>
+        <div className={styles.section}>
+          <div className={styles.text}>
+            <h3>Gain de Temps et Efficacité</h3>
+            <p>Google Ads est un outil complexe nécessitant une configuration minutieuse. Confier la gestion de vos campagnes à une agence spécialisée vous permet de vous concentrer sur votre cœur de métier, tout en bénéficiant d'une expertise avancée.</p>
+          </div>
+          
+        </div>
+        <div className={styles.section}>
+          <div className={styles.text}>
+            <h3>Outils Avancés et Analyses Approfondies</h3>
+            <p>Les agences Google Ads utilisent des outils avancés comme SEMrush, SEISO et Catchr pour analyser et optimiser vos campagnes en continu, assurant ainsi un rendement maximal.</p>
+          </div>
+        </div>
+        <div className={styles.section}>
+          <div className={styles.text}>
+            <h3>Expérience et Optimisation Financière</h3>
+            <p>L'expérience d'une agence permet d'optimiser chaque euro dépensé, vous conseillant sur l'augmentation ou la réduction de votre budget en fonction des saisonnalités et des tendances de votre secteur.</p>
+          </div>
+        </div>
+      </section>
+      <section className={`${styles.presentation} ${styles.appear}`}>
+        <h2>Comment Travaille une Agence Google Ads ? <img className={styles.fusee} src="/images/fusée.gif" alt="Croissance des clients" /></h2>
+        <div className={styles.section}>
+          <div className={styles.text}>
+            <h3>Cadrage et Stratégie</h3>
+            <p>Un expert Google Ads analyse votre marché, vos objectifs et votre public cible pour établir une stratégie sur mesure, axée sur des mots-clés et segments d’audience pertinents afin d’optimiser votre ROI.</p>
+          
+          </div>
+          <div className={styles.image}>
+            <img src="/images/adsgif.gif" alt="Croissance des clients" />
+          </div>
 
-      <div className={styles.container}>
-        <h1>Nos Forfaits Google Ads</h1>
+        </div>
+        <div className={styles.section}>
+          <div className={styles.text}>
+            <h3>Mise en Place et Création des Annonces</h3>
+            <p>La rédaction d’annonces percutantes, en phase avec votre marque, et l’intégration d’extensions d’annonces sont des éléments clés pour augmenter la visibilité et le taux de clics.</p>
+          </div>
+          
+        </div>
+        <div className={styles.section}>
+          <div className={styles.text}>
+            <h3>Analyse et Optimisation</h3>
+            <p>Après le lancement, l'agence surveille les performances des annonces, effectue des ajustements, teste de nouvelles stratégies et optimise les mots-clés et les extensions d’annonces pour maximiser les résultats.</p>
+          </div>
+        </div>
+      </section>
+      </main>
+
+      <div className={`${styles.container} ${styles.appear}`}> 
+        <h1>Nos Forfaits Google Ads <img className={styles.fusee2} src="/images/fusée.gif" alt="Croissance des clients" /></h1>
         <p>Découvrez nos différents forfaits pour la gestion de vos campagnes Google Ads.</p>
         <div className={styles.forfaits}>
           {forfaits.map((forfait) => (
             <div key={forfait.id} className={styles.forfait}>
-              <h2>{forfait.titre}</h2>
+              <h2>{forfait.titre} {forfait.prix}</h2>
               <p>{forfait.description}</p>
               <ul>
                 {forfait.elements.map((element, index) => (
                   <li key={index}>{element}</li>
                 ))}
               </ul>
-              <p><strong>{forfait.prix}</strong></p>
-              <button onClick={() => { setSelectedForfait(forfait); setShowModal(true); }}>Acheter</button>
+              
+              <button onClick={() => { setSelectedForfait(forfait); setShowModal(true); }}>Lancer ma compagne</button>
             </div>
           ))}
         </div>
@@ -104,7 +209,8 @@ const GoogleAds = () => {
         <div className={styles.modal}>
           <div className={styles.modalContent}>
             <span className={styles.close} onClick={() => setShowModal(false)}>&times;</span>
-            <h2>Demande de Forfait: {selectedForfait.titre}</h2>
+            <h2>Demande de Forfait: {selectedForfait.titre}<img className={styles.fusee3} src="/images/fusée.gif" alt="Croissance des clients" /></h2>
+            <h4>Merci de compléter vos informations, notre service commercial prendra un rendez-vous avec vous pour confirmer vos informations et démarrer votre campagne google ads !</h4>
             <form className={styles.form} onSubmit={handleSubmit}>
               <label>
                 Nom de la Société:
@@ -123,9 +229,11 @@ const GoogleAds = () => {
           </div>
         </div>
       )}
+      
       <Reviews />
       <Faq />
       <Contact />
+    
     </main>
   );
 };
